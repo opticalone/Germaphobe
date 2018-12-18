fa27b5a9a4ae543b37e171079db1228855cf164c
Michael Kaplor <mkaplor@gmail.com>
2018-12-18 12:07:02 -0800

    added germs, controller, shooting, basic af ai

---
+++ Assets/programming/_scripts/JanitorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanitorController : MonoBehaviour {
+
+    public float speed = 10f;
+    public float lookSensitivity = 3;
+
+    //component cache
+    private JanitorMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<JanitorMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float xMove = Input.GetAxis("Horizontal");                          //calc movement
+        float yMove = Input.GetAxis("Vertical");
+        Vector3 _moveHorizontal = transform.right * xMove;
+        Vector3 _moveVertical = transform.forward * yMove;                  
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical) * speed;      //calculate final movement vector
+        motor.Move(_velocity);                                              //apply movement
+
+
+        float _yRot = Input.GetAxisRaw("Mouse X");                          // calculate player rotation
+        Vector3 _rotation = new Vector3(0, _yRot,0)* lookSensitivity;       //add sensitivities
+        motor.Rotate(_rotation);                                            //do it
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");                          //calculate camera rotate
+        float _camRotx = _xRot * lookSensitivity;                           //add  sense
+        motor.CameraRotate(-_camRotx);                                       //do
+	}
+}
---
+++ Assets/programming/_scripts/JanitorMotor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanitorMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 vel = Vector3.zero;
+    private Vector3 rot = Vector3.zero;
+    private float camRotX = 0;
+    private float currentCamRotX = 0;
+
+    [SerializeField]
+    private float cameraRotateLimit = 89f;
+    private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+    public void Move(Vector3 _vel) //get move vector
+    {
+        vel = _vel;
+    }
+    public void Rotate(Vector3 _rot) // get rotation vector
+    {
+        rot = _rot;
+    }
+    public void CameraRotate(float _camRotate)
+    {
+        camRotX = _camRotate;
+    }
+	// Update is called once per frame
+	void Update () {
+        doRotate();
+	}
+    private void LateUpdate()
+    {
+        doMove();
+    }
+
+    void doMove()
+    {
+        if (vel != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + vel * Time.fixedDeltaTime);
+        }        
+    }
+    void doRotate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rot));
+        if (cam!=null)
+        {
+            currentCamRotX += camRotX;
+            currentCamRotX = Mathf.Clamp(currentCamRotX, -cameraRotateLimit, cameraRotateLimit);
+            cam.transform.localEulerAngles = new Vector3(currentCamRotX, 0, 0);
+        }
+
+    }
+}
